74a314d1a131bc914e289c8d34fa2667c6f5e270
Adrian <tamkayeung.adrian@gmail.com>
2021-12-18 23:55:07 +0900

    update newer version

--- Custom_Error/improved_version.py
+++ Custom_Error/improved_version.py
@@ -15,28 +15,32 @@ def simle_error(screen):
         BannerText(screen, ImageFile("img/simle.png", screen.height - 2, colours=screen.colours),
               0, 0),
         Print(screen,
-              FigletText("www ERROR www",
-                         font='banner3' if screen.width > 80 else 'banner'),
+              FigletText(
+                "www ERROR www",
+                font='banner3' if screen.width > 80 else 'banner'
+                ),
               screen.height//2-3,
               colour=Screen.COLOUR_RED, bg=7 if screen.unicode_aware else 0)
     ]
     scenes.append(Scene(effects))
-
     screen.play(scenes, stop_on_resize=True)
 
 def error_404(screen):
     scenes = []
     effects = [
         Print(screen,
-              ColourImageFile(screen, "img/404.png", screen.height-2,
-                              uni=screen.unicode_aware,
-                              dither=screen.unicode_aware),
-              0,
-              stop_frame=200)
+              ColourImageFile(
+                screen, 
+                "img/404.png", 
+                screen.height-2,
+                uni=screen.unicode_aware,
+                dither=screen.unicode_aware
+                ),
+            0,
+            stop_frame=200)
     ]
     scenes.append(Scene(effects))
     screen.play(scenes, stop_on_resize=True)
-
 
 def CustomError(demo):
     while True:
@@ -47,7 +51,6 @@ def CustomError(demo):
                 pass    
 
 if __name__ == "__main__":
-    
     try:
         1 + "str"
     except:
