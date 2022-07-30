cd899e4e4418936641522d46086d71037806ce65
AIF-adrian <tamkayeung.adrian@gmail.com>
2022-07-30 17:56:34 +0900

    add stuff

--- teamSpirit/run.py
+++ teamSpirit/run.py
@@ -30,18 +30,18 @@ if __name__ == '__main__':
     options = uc.ChromeOptions()
     options.add_argument("--ignore-certificate-error")
     options.add_argument("--ignore-ssl-errors")
-    # options.add_argument('--headless')
+    options.add_argument('--headless')
 
     # e.g. Chrome path in Mac =/Users/x/Library/xx/Chrome/Default/
     # options.add_argument( "--user-data-dir=<Your chrome profile>")
     # options.add_argument("--user-data-dir=D:/Adrian/User_Data")
-    options.add_argument(r"--user-data-dir=D:\Python\git\Random_Scripts\teamSpirit\User_Data")
-    options.add_argument('--profile-directory=Profile 1')
+    # options.add_argument(r"--user-data-dir=D:\Python\git\Random_Scripts\teamSpirit\User_Data")
+    # options.add_argument('--profile-directory=Profile 1')
 
     driver = uc.Chrome(options=options)
-
+    driver.maximize_window()
     time.sleep(4)
-    url='https://stco-group.my.salesforce.com/home/home.jsp'
+    url='https://login.salesforce.com/'
     # url = "https://google.com"
     driver.get(url)
     
@@ -56,37 +56,54 @@ if __name__ == '__main__':
     # driver.find_element_by_link_text("TeamSpirit").click()
     # time.sleep(200)
         
-    # menu1 = driver.find_element_by_xpath("//*[@id='gbwa']/div[1]/a")
-    # menu1.click() 
-    # menu2 = driver.find_element_by_xpath("//*[@id='gb23']/span[1]")
-    # menu2.click() 
+    user_name = driver.find_element_by_xpath("//*[@id='username']")
+    password = driver.find_element_by_xpath("//*[@id='password']")
+    
+    user_name.send_keys("kayeungadrian.tam.mr@ambl.group") 
+    password.send_keys("Maths@152631") 
+    
+    
+    menu2 = driver.find_element_by_xpath("//*[@id='Login']")
+    menu2.click() 
+
+
     time.sleep(2)
 
-    # driver.find_element_by_xpath("//button[@class='button mb24 secondary wide']").click()
-    # driver.find_element_by_xpath('//button[contains(text(), "Google Workspace")]').click()
 
-
-    ts = driver.find_element(By.XPATH, "//*[contains(@class, 'button mb24 secondary wide') and contains(., 'Google Workspace')]")
-    ts.send_keys("\n")
-
-    time.sleep(5)
-    # driver.save_screenshot("test2.png")
-    # time.sleep(200)
     ks = driver.find_element_by_xpath("//a[@title='勤怠打刻']")
     ks.send_keys("\n")
-    time.sleep(5)
-    # ids = driver.find_elements_by_xpath('//button[@id]')
-    ids = driver.find_element_by_xpath("//*[@id='remarks']")
-    # ids.click()
 
-    # for id in ids:
-    #     print(id)
-    #     print(id.get_attribute('class'))
 
-    # driver.find_element_by_xpath("//input[@title='出社打刻']")
-    # driver.find_element_by_xpath("//input[@type='button' and @value='出勤']")
-    # driver.find_element_by_id("pushStart").click()
+
+    time.sleep(1)
+
+    iframe = driver.find_element_by_xpath("//*[@id='06610000000rDxW']")
+    driver.switch_to.frame(iframe)
+
+
+    time.sleep(1)
+
+    tmp = driver.find_element_by_xpath("//*[@id='btnStInput']")
+    # tmp.click() 
+
+    time.sleep(1)
+
+    driver.switch_to.default_content()
 
     
-    time.sleep(2)
-    driver.close()
+
+    ts = driver.find_element(By.XPATH, "//*[contains(@id, '01r10000000bu9k_Tab')]")
+    ts.click()
+
+
+    time.sleep(1)
+
+    date = "dateRow2022-05-09"
+
+    ts = driver.find_element(By.XPATH, "//*[contains(@id, '" + date + "')]")
+
+    ttt = (ts.text).replace("\n", " ").split(" ")
+
+    print(ttt)
+
+    driver.save_screenshot("test2.png")
